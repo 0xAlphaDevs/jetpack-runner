d7ca8595023687849b5a2eb7e86718872a1986d1
Harsh Tyagi <writetoharshtyagi@gmail.com>
2025-07-11 17:35:24 +0530

    feat: add play component and add utils folder

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+export interface JetpackRunnerPlayer {
+  id: string;
+  username: string;
+  jetpack_token_balance: number;
+  high_score: number;
+  total_distance: number;
+  potions_collected: number;
+  mystery_boxes: any[];
+  game_missions_completed: any[];
+}
